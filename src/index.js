e2ff5d24753d18b5235ec4b36cdc69977f87538b
Mahesh Kannan <maheshkannank@koraltech.in>
2025-02-08 06:43:06 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const heading = <h1>Mahesh Kannan</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
