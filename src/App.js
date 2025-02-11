5fb372bb1af58f3f602b09253d08ae1dcae85e72
Mahesh Kannan <maheshkannank@koraltech.in>
2025-02-11 22:10:28 +0530

    advanced css day1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+import { Header } from './components';
+
+const App = () => {
+  return (
+    <body>
+      <Header></Header>
+    </body>
+  );
+};
+
+export default App;
