0b7a68cf89ef5d3f11179bfd552f98441d08c1bf
Mahesh Kannan <maheshkannank@koraltech.in>
2025-02-12 23:01:31 +0530

    Advanced CSS Day 2

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,16 @@
 import './index.css';
+import logo from '../../assets/images/logo-white.png';
 const Header = () => {
-  return <header>This is Header</header>;
+  return (
+    <header>
+      <div className='h_logo_container'>
+        <img src={logo} alt='Logo' className='h_logo_img' />
+      </div>
+      <div className='h_heading_container'>
+        <h1 className='h_primary_heading_text'>{'outdoors'}</h1>
+        <h1 className='h_secondary_heading_text'>{'is where life happens'}</h1>
+      </div>
+    </header>
+  );
 };
 export default Header;
