95258bd3ac27a27fc0616b5d6a105df3c4a80554
Mahesh Kannan <maheshkannank@koraltech.in>
2025-02-23 22:34:49 +0530

    Addvanced CSS Day 13

--- src/components/feature/index.js
+++ src/components/feature/index.js
@@ -2,11 +2,12 @@ import './index.scss';
 import { Map, Compass, Globe, Heart } from '../../assets/svgs';
 
 const Feature = () => {
-  const featureCard = ({ Svg }) => {
+  const featureCard = ({ Svg, Heading = '' }) => {
     return (
       <div className='feature_main_card'>
-        <Svg />
-        <p className='feature_main_card_'>
+        <Svg strokeColor="#28b485"/>
+        <h3 className='feature_main_card_heading'>{Heading}</h3>
+        <p className='feature_main_card_text'>
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout.
         </p>
@@ -15,10 +16,12 @@ const Feature = () => {
   };
   return (
     <div className='feature'>
-      <div className='feature_main'>{featureCard({ Svg: Map })}</div>
-      <div className='feature_main'>{featureCard({ Svg: Globe })}</div>
-      <div className='feature_main'>{featureCard({ Svg: Heart })}</div>
-      <div className='feature_main'>{featureCard({ Svg: Compass })}</div>
+      <div className='feature_main'>
+        {featureCard({ Svg: Map, Heading: 'Find Your Way' })}
+        {featureCard({ Svg: Compass, Heading: 'Meet Nature' })}
+        {featureCard({ Svg: Globe, Heading: 'Explore the World' })}
+        {featureCard({ Svg: Heart, Heading: 'Live a Healthier Life' })}
+      </div>
     </div>
   );
 };
