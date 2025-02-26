722d4a9b300f9aa9e57349363b5afe9b038dc824
Mahesh Kannan <maheshkannank@koraltech.in>
2025-02-26 21:38:42 +0530

    Addvanced CSS Day 16

--- src/components/tourcard/index.js
+++ src/components/tourcard/index.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import HoverExpandButton from '../hoverexpandbutton';
 
 const TourCard = ({
   days = 1,
@@ -6,11 +7,34 @@ const TourCard = ({
   guidesCount = '1',
   sleepArea = 'Provided Tents',
   difficulty = 'Easy',
+  title1 = '',
+  title2 = '',
+  price = 0,
+  primaryColor = '',
+  secondaryColor = '',
 }) => {
+  console.log(primaryColor);
   return (
     <div className='tour_card'>
       <div className='tour_card_side tour_card_side-front'>
-        <div className='tour_card_side_img'></div>
+        <div className='tour_card_side_img'>
+          <h1
+            className='tour_card_side_img_title1'
+            style={{
+              backgroundImage: `linear-gradient(to right bottom, var(${primaryColor}), var(${secondaryColor}))`,
+            }}
+          >
+            {title1}
+          </h1>
+          <h1
+            className='tour_card_side_img_title2'
+            style={{
+              backgroundImage: `linear-gradient(to right bottom, var(${primaryColor}), var(${secondaryColor}))`,
+            }}
+          >
+            {title2}
+          </h1>
+        </div>
         <div className='tour_card_side_details'>
           <label className='tour_card_side_details_label'>{`${days} day tour`}</label>
           <label className='tour_card_side_details_label'>{`Up to ${maxStrength} people`}</label>
@@ -19,8 +43,14 @@ const TourCard = ({
           <label className='tour_card_side_details_label'>{`Difficulty: ${difficulty}`}</label>
         </div>
       </div>
-      <div className='tour_card_side tour_card_side-back'>
-        <h1>Mithil Yaazhan</h1>
+      <div className='tour_card_side tour_card_side_back'>
+        <div className='tour_card_side_back_price'>
+          <span className='tour_card_side_back_price_only_text'>Only</span>
+          <span className='tour_card_side_back_price_text'>{`$ ${price}`}</span>
+        </div>
+        <div className='tour_card_side_back_button'>
+          <HoverExpandButton text='Book Now!' />
+        </div>
       </div>
     </div>
   );
