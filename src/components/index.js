6bca7725f244a92363070800371d517dbf257472
Mahesh Kannan <maheshkannank@koraltech.in>
2025-03-03 21:30:36 +0530

    Addvanced CSS Day 19

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Tours from './tours';
 import About from './about';
 import Header from './header';
 import Feature from './feature';
+import StorySection from './storysection';
 
-export { Tours, About, Header, Feature };
+export { Tours, About, Header, Feature, StorySection };
---
+++ src/components/storysection/index.js
@@ -0,0 +1,12 @@
+import './index.scss';
+import Testimonial from '../testimonial';
+
+const StorySection = () => {
+  return (
+    <div className='stories'>
+      <Testimonial />
+    </div>
+  );
+};
+
+export default StorySection;
