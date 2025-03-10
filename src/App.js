921ba36e8671c6d4d6969070c162394d3f9929c1
Mahesh Kannan <maheshkannank@koraltech.in>
2025-03-10 22:34:20 +0530

    Addvanced CSS Day 22

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Tours,
   About,
   Header,
+  Footer,
   Feature,
   Booking,
   StorySection,
@@ -19,6 +20,7 @@ const App = () => {
       <Tours />
       <StorySection />
       <Booking />
+      <Footer />
     </body>
   );
 };
